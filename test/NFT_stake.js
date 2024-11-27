6c848c776b3f7ac5ef53bd55250dbceb29245a83
The Dead Man Walking <tshiburi123@student.wethinkcode.co.za>
2024-11-27 11:51:34 +0200

    saving the metadata progress which is done

--- test/NFT_stake.js
+++ test/NFT_stake.js
@@ -2,24 +2,29 @@ const { expect } = require("chai");
 const hre = require("hardhat");
 const { time, loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
-describe("NFT_stake", function () {
+describe("Deploy", function () {
     
     async function deployNFTStakeFixture() {
         const [owner, addr1, addr2] = await ethers.getSigners();
 
-        const erc721ContractDeployed = await hre.ethers.deployContract("MyNFT", "MNFT","base_url");
+        const ERC721Contract = await ethers.getContractFactory("ERC721_Contract");
 
-        const zarDeployed = await hre.ethers.deployContract();
 
-        const nftStake = await hre.ethers.deployContract("NFT_stake",erc721ContractDeployed, zarDeployed);
+        const erc721ContractDeployed =  await ERC721Contract.deploy("MyNFT", "MNFT","ipfs://QmWWrUMWCUYTLR24KcvmRfCDR3JFzZHfTmG5PBifR2R4RF/");
+
+        const zarDeployed = await hre.ethers.deployContract("ZAR");
+
+        const nftStake = await hre.ethers.deployContract("NFT_stake",erc721ContractDeployed.target, zarDeployed.target);
     
         // Fixtures can return anything you consider useful for your tests
-        return {nftStake, erc721ContractDeployed, zarDeployed};
+        return {nftStake, erc721ContractDeployed, zarDeployed, owner};
     }
 
     it("Should be able to stake" ,async function() {
-        const {nftStake , erc721, zar} = await loadFixture(deployNFTStakeFixture);
-        // erc721.
+        const {nftStake , erc721, zar, owner} = await loadFixture(deployNFTStakeFixture);
+        
+        erc721.mint(owner);
 
+    
     });
 });
